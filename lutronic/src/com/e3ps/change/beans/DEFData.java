45e16c265c6a5b1f251213032f83dfc55a54eb02
PLMDEV\Administrator <jhkim@3pchain.co.kr>
2023-07-03 14:23:30 +0900

    no message

--- lutronic/src/com/e3ps/change/beans/DEFData.java
+++ lutronic/src/com/e3ps/change/beans/DEFData.java
@@ -15,25 +15,13 @@ import wt.org.WTUser;
 @Getter
 @Setter
 public class DEFData {
-	
-//	public String oid;
-//	public String rootOid;
-//	public String name;
-//	public String name_eng;
-//	public String step;
-//	public String activityType;
-//	public String activityName;
-//	public WTUser activeUser;
-//	public String activeUserOid;
-//	public String activeUserName;
-//	public int sortNumber;
-//	public String description;
-//	public String viewDescription;
+
 	private String oid;
 	private String rootOid;
 	private String name;
 	private String name_eng;
 	private String step;
+	private String stepName;
 	private String activityType;
 	private String activityName;
 	private WTUser activeUser;
@@ -42,22 +30,27 @@ public class DEFData {
 	private int sortNumber;
 	private String description;
 	private String viewDescription;
-	
+
 	public DEFData(EChangeActivityDefinition def) {
 		setOid(CommonUtil.getOIDString(def));
 		setRootOid(CommonUtil.getOIDString(def.getRoot()));
 		setName(StringUtil.checkNull(def.getName()));
 		setName_eng(StringUtil.checkNull(def.getName_eng()));
 		setStep(StringUtil.checkNull(def.getStep()));
+		NumberCode stepCode = NumberCodeHelper.service.getNumberCode("EOSTEP", getStep());
+		if (stepCode != null) {
+			setStepName(stepCode.getName());
+		}
+
 		setActivityType(StringUtil.checkNull(def.getActiveType()));
-		setActivityName(ChangeUtil.getActivityName(this.activityType));
-		setActiveUser(def.getActiveUser());
-		if(activeUser != null){
-			setActiveUserOid(CommonUtil.getOIDString(activeUser));
-			setActiveUserName(activeUser.getFullName());
+		setActivityName(ChangeUtil.getActivityName(getActivityType()));
+		if (def.getActiveUser() != null) {
+			setActiveUser(def.getActiveUser());
+			setActiveUserOid(CommonUtil.getOIDString(getActiveUser()));
+			setActiveUserName(getActiveUser().getFullName());
 		}
 		setSortNumber(def.getSortNumber());
 		setDescription(StringUtil.checkNull(def.getDescription()));
-		setViewDescription(WebUtil.getHtml(description));
+		setViewDescription(WebUtil.getHtml(getDescription()));
 	}
 }
