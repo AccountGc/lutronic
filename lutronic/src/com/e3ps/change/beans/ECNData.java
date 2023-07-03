10c44243268a3be317260ba491dde53b21707b1b
PLMDEV\Administrator <jhkim@3pchain.co.kr>
2023-07-03 16:56:49 +0900

    getSet작업중

--- lutronic/src/com/e3ps/change/beans/ECNData.java
+++ lutronic/src/com/e3ps/change/beans/ECNData.java
@@ -4,74 +4,29 @@ import com.e3ps.change.EChangeNotice;
 import com.e3ps.common.message.Message;
 import com.e3ps.common.util.DateUtil;
 
+import lombok.Getter;
+import lombok.Setter;
+import wt.org.WTUser;
 
-
+@Getter
+@Setter
 public class ECNData extends EOData{
 	
-	public String oid;
-	public String number;
-	public String name;
-	public String state;
-	public String creator;
-	public String createDateD;
+	private String oid;
+	private String number;
+	private String name;
+	private String state;
+	private String creator;
+	private String createDateD;
 	
 	public ECNData(final EChangeNotice ecn) {
 		super(ecn);
 		
-		this.oid = ecn.getPersistInfo().getObjectIdentifier().toString();
-		this.number = ecn.getEoNumber();
-		this.name = ecn.getEoName();
-		this.state = ecn.getLifeCycleState().getDisplay(Message.getLocale());
-		this.creator = ecn.getCreatorFullName();
-		this.createDateD = DateUtil.getDateString(ecn.getCreateTimestamp(), "d");
+		setOid(ecn.getPersistInfo().getObjectIdentifier().toString());
+		setNumber(ecn.getEoNumber());
+		setName(ecn.getEoName());
+		setState(ecn.getLifeCycleState().getDisplay(Message.getLocale()));
+		setCreator(ecn.getCreatorFullName());
+		setCreateDateD(DateUtil.getDateString(ecn.getCreateTimestamp(), "d"));
 	}
-
-	public String getOid() {
-		return oid;
-	}
-
-	public void setOid(String oid) {
-		this.oid = oid;
-	}
-
-	public String getNumber() {
-		return number;
-	}
-
-	public void setNumber(String number) {
-		this.number = number;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getCreator() {
-		return creator;
-	}
-
-	public void setCreator(String creator) {
-		this.creator = creator;
-	}
-
-	public String getCreateDateD() {
-		return createDateD;
-	}
-
-	public void setCreateDateD(String createDateD) {
-		this.createDateD = createDateD;
-	}
-	
 }	
