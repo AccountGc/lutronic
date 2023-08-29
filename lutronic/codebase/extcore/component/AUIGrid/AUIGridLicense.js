39820d332a6332335254a9b979d6884c0baa073c
Jangcccci <jangcccci@gmail.com>
2023-08-29 13:34:20 +0900

    그리드 라이센스

--- lutronic/codebase/extcore/component/AUIGrid/AUIGridLicense.js
+++ lutronic/codebase/extcore/component/AUIGrid/AUIGridLicense.js
@@ -6,5 +6,6 @@
  * www.auisoft.net
  */
  /* eslint-disable */
+ 
 var AUIGridLicense = "eyJjdCI6ImNncXJWdDFneFQ0WGZoQVFQSENpSmJCTFJLeTUrV25WSDc2NGR2SkJBSUhSelZXaUtVWVoxUWpjY1pqSC9PMFROeHJRVHUzU203S2dpYUw2L3VYLzlZVFUzNWt1Wk5ENU9jLzVNTDQ3NHBvRmF1VHpzSEo3cThDYW9TT210QzNsIiwiaXYiOiI1MjRhYmIxOTY1Mzc2MmQ1ZmM0ZGQxMjhjNzY5N2EwNiIsInMiOiJkNDJjODk0NTlhMWVlZjBlIn0=";
 if (typeof window !== "undefined") window.AUIGridLicense = AUIGridLicense;
