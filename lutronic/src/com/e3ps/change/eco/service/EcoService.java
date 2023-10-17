57d97f8d76cd9e606b13b7b6da04e6db3264377c
lmc <lmc0129@j-iot.co.kr>
2023-10-17 15:47:56 +0900

    eco 수정

--- lutronic/src/com/e3ps/change/eco/service/EcoService.java
+++ lutronic/src/com/e3ps/change/eco/service/EcoService.java
@@ -1,6 +1,7 @@
 package com.e3ps.change.eco.service;
 
 import com.e3ps.change.eco.dto.EcoDTO;
+import com.e3ps.change.eo.dto.EoDTO;
 
 import wt.method.RemoteInterface;
 
@@ -12,4 +13,8 @@ public interface EcoService {
 	 */
 	public abstract void create(EcoDTO dto) throws Exception;
 
+	/**
+	 * ECO 수정
+	 */
+	public abstract void modify(EcoDTO dto) throws Exception;
 }
