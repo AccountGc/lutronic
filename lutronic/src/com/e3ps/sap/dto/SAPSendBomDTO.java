59a21d5ae2def811daa7b03c2fae677f74b27117
Jangcccci <jangcccci@gmail.com>
2023-12-26 15:37:45 +0900

    no message

--- lutronic/src/com/e3ps/sap/dto/SAPSendBomDTO.java
+++ lutronic/src/com/e3ps/sap/dto/SAPSendBomDTO.java
@@ -36,7 +36,7 @@ public class SAPSendBomDTO {
 		// 이전 자식
 		WTPart pre_child = SAPHelper.manager.getPre(child, eco);
 
-		if (pre_childe != null) {
+		if (pre_child != null) {
 			System.out.println("child + " + child.getNumber() + ", pre_child=" + pre_child.getNumber());
 		} else {
 			System.out.println("child + " + child.getNumber() + ", pre_child=" + pre_child);
