47b0d641853b8fec6c2344541556bf030de2185c
Jangcccci <jangcccci@gmail.com>
2023-12-28 15:25:21 +0900

    no message

--- src/com/e3ps/admin/form/service/FormTemplateHelper.java
+++ src/com/e3ps/admin/form/service/FormTemplateHelper.java
@@ -105,8 +105,10 @@ public class FormTemplateHelper {
 			return "설계 및 검증 검토 회의록";
 		} else if ("PP완료여부회의".equals(clazz)) {
 			return "PP 완료 여부 검토 회의록";
-		} else if("테스트보고서".equals(clazz) {
+		} else if ("테스트보고서".equals(clazz)) {
 			return "보고서(테스트)";
+		} else if ("제3자검증의뢰서".equals(clazz)) {
+			return "";
 		}
 		return "";
 	}
