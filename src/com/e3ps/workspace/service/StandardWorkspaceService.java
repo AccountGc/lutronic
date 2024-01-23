552eef11ca9f2e07de539c20c4b22203133bcc0d
Jangcccci <jangcccci@gmail.com>
2024-01-23 14:22:55 +0900

    no message

--- src/com/e3ps/workspace/service/StandardWorkspaceService.java
+++ src/com/e3ps/workspace/service/StandardWorkspaceService.java
@@ -110,6 +110,7 @@ public class StandardWorkspaceService extends StandardManager implements Workspa
 		master = (ApprovalMaster) PersistenceHelper.manager.save(master);
 
 		master = (ApprovalMaster) PersistenceHelper.manager.refresh(master);
+		data = (WorkData) PersistenceHelper.manager.refresh(data);
 
 		// 마스터 세팅
 		data.setAppMaster(master);
